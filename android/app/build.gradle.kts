0b282d80935f498dc67490d6e892b7b369875180
Navin Kumar Verma <kumarnavinverma7@gmail.com>
2026-07-26 23:13:43 +0530

    chore: bump app version to 1.0.5+6 and enable core library desugaring in Android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -69,5 +70,5 @@ flutter {
 }
 
 dependencies {
-    // Add any native Android dependencies here
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
